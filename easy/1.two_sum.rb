4243257a4fb07a01d8a81e5f4f79fd7a08dfa70f
chasumaru <ptj.chasu@gmail.com>
2025-06-08 22:57:14 +0900

    1.two_sum_best_solution

--- easy/1.two_sum.rb
+++ easy/1.two_sum.rb
@@ -1,6 +1,9 @@
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
+
+# 基本形 時間計算量：O(n^2)
+
 def two_sum(nums, target)
   l = nums.size
 
@@ -9,4 +12,21 @@ def two_sum(nums, target)
       return [i, j] if nums[i] + nums[j] == target
     end
   end
+end
+
+# 最適解 → 時間計算量：O(n)
+# 補数のHashMapを利用
+
+def two_sum(nums, target)
+    ht = {}
+    
+    nums.each_with_index do |num, i|
+        if ht.key?(target - num)
+            return [ht[target - num], i]
+        end
+        
+        ht[num] = i
+    end
+    
+    []
 end
