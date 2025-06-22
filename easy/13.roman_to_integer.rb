d4083e36ebd9478e93afbfdae283b45f45505a8c
chasumaru <ptj.chasu@gmail.com>
2025-06-22 22:15:42 +0900

    250622

--- easy/13.roman_to_integer.rb
+++ easy/13.roman_to_integer.rb
@@ -27,3 +27,29 @@ def roman_to_int(s)
 
   total
 end
+
+
+# 2文字一致での処理
+def roman_to_int(s)
+  map = {
+    "IV" => 4, "IX" => 9, "XL" => 40,
+    "XC" => 90, "CD" => 400, "CM" => 900,
+    "I" => 1, "V" => 5, "X" => 10,
+    "L" => 50, "C" => 100, "D" => 500, "M" => 1000
+  }
+
+  i = 0
+  total = 0
+
+  while i < s.length
+    if i + 1 < s.length && map[s[i, 2]]
+      total += map[s[i, 2]]
+      i += 2
+    else
+      total += map[s[i]]
+      i += 1
+    end
+  end
+
+  total
+end
